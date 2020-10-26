653f70df550ad820eebda1a7dbea49005eb29a5b
Warren Whelan <warrenjw17@gmail.com>
2020-10-26 14:36:00 +1300

    Get process id

--- SimpleInjector/SimpleInjector.cpp
+++ SimpleInjector/SimpleInjector.cpp
@@ -4,6 +4,11 @@
 
 int main()
 {
+	const wchar_t* process = L"csgo.exe";	// Process to inject into
+	const wchar_t* file = L"InHop.dll";		// Dll to inject
+
+	DWORD processId = 0u;
+
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 	if (hSnapshot == INVALID_HANDLE_VALUE)
@@ -12,11 +17,41 @@ int main()
 		return 0;
 	}
 
-	PROCESSENTRY32 pEntry;
-	pEntry.dwSize = sizeof(PROCESSENTRY32);
+	PROCESSENTRY32 processEntry;
+	processEntry.dwSize = sizeof(PROCESSENTRY32);
 
-	if (Process32First(hSnapshot, &pEntry))
+	if (Process32First(hSnapshot, &processEntry))
 	{
-
+		do
+		{
+			if (!wcscmp(processEntry.szExeFile, process))
+			{
+				processId = processEntry.th32ProcessID;
+				CloseHandle(hSnapshot);
+				break;
+			}
+		} while (Process32Next(hSnapshot, &processEntry));
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	CloseHandle(hSnapshot);
+	return 0;
 }
